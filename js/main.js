5fca79bc8129143d6c9bc89e56fe4621bd111898
plant004 <plant004@gmail.com>
2012-10-05 23:14:19 +0900

    いくつかの関数を実装

--- js/main.js
+++ js/main.js
@@ -34,6 +34,7 @@ window.onload = function(){
 		T_BLOCK : 7,
 		WALL : 8
 	};
+	
 
 	// 位置を表すクラス
 	game.Position = enchant.Class.create( {
@@ -65,8 +66,8 @@ window.onload = function(){
 			}
 		},
 		setXY : function( x, y ){
-			this.x.set( x );
-			this.y.set( y );
+			this._x = x;
+			this._y = y;
 		}
 	} );
 
@@ -83,54 +84,55 @@ window.onload = function(){
 		init : function( type ){
 			switch( type ) {
 				case game.BLOCK_TYPE.NO_BLOCK :
-					this.rotate.set( 1 );
+					this._rotate = 1;
 					this.pos[0].setXY( 0, 0 );
 					this.pos[1].setXY( 0, 0 );
 					this.pos[2].setXY( 0, 0 );
 					break;
 				case game.BLOCK_TYPE.Z_BLOCK :
-					this.rotate.set( 2 );
+					this._rotate = 2;
 					this.pos[0].setXY( 0, -1 );
 					this.pos[1].setXY( -1, 0 );
 					this.pos[2].setXY( -1, 1 );
 					break;
 				case game.BLOCK_TYPE.L_BLOCK :
-					this.rotate.set( 4 );
+					this._rotate = 4;
 					this.pos[0].setXY( 0, -1 );
 					this.pos[1].setXY( 0, 1 );
 					this.pos[2].setXY( -1, 1 );
 					break;
 				case game.BLOCK_TYPE.O_BLOCK :
-					this.rotate.set( 1 );
+					this._rotate = 1;
 					this.pos[0].setXY( 0, 1 );
 					this.pos[1].setXY( 1, 0 );
 					this.pos[2].setXY( 1, 1 );
 					break;
 				case game.BLOCK_TYPE.S_BLOCK :
-					this.rotate.set( 2 );
+					this._rotate = 2;
 					this.pos[0].setXY( 0, -1 );
 					this.pos[1].setXY( 1, 0 );
 					this.pos[2].setXY( 1, 1 );
 					break;
 				case game.BLOCK_TYPE.I_BLOCK :
-					this.rotate.set( 2 );
+					this._rotate = 2;
 					this.pos[0].setXY( 0, -1 );
 					this.pos[1].setXY( 0, 1 );
 					this.pos[2].setXY( 0, 2 );
 					break;
 				case game.BLOCK_TYPE.J_BLOCK :
-					this.rotate.set( 4 );
+					this._rotate = 4;
 					this.pos[0].setXY( 0, -1 );
 					this.pos[1].setXY( 0, 1 );
 					this.pos[2].setXY( 1, 1 );
 					break;
 				case game.BLOCK_TYPE.T_BLOCK :
-					this.rotate.set( 4 );
+					this._rotate = 4;
 					this.pos[0].setXY( 0, -1 );
 					this.pos[1].setXY( 1, 0 );
 					this.pos[2].setXY( -1, 0 );
 					break;
-			}
+				
+			};
 		},
 		rotate : {
 			get : function() {
@@ -142,12 +144,17 @@ window.onload = function(){
 		},
 	} );
 
+	game.blocks = new Array();
+	for( var type; type < game.BLOCK_TYPE_NUM; type++ ) {
+		game.blocks[type] = new game.Block( type );
+	}
+
 	// ブロックの状態を示すクラス
 	game.Status = enchant.Class.create( {
 		initialize : function( x, y, rotate, type ) {
 			this.pos = new game.Position( x, y );
-			this.rotate.set( rotate );
-			this.type.set( type );
+			this._rotate = rotate;
+			this._type = type;
 		},
 		type : {
 			get : function() {
@@ -192,53 +199,146 @@ window.onload = function(){
 					}
 				}
 			}
+		},
+		
+		/**
+		 * ブロックを置く
+		 * @param {game.Status} [status] 設置するブロックの状態
+		 * @param {Boolean} [action] 実際にブロックを置くかどうか。trueの場合は設置、falseの場合は設置できるか確認後設置
+		 * @return {Boolean} ブロックが置けた場合はtrue、そうでなければfalse
+		 */
+		putBlock : function( status, action ) {
+			// 既にブロックが置かれている場合は終了
+			if( game.BLOCK_TYPE.NO_BLOCK < this._field[status.x][status.y] &&
+				this._field[status.x][status.y] <= game.BLOCK_TYPE.WALL ) return false;
+
+			if( action ) {
+				this._field[status.x][status.y] = status.type;
+			}
+			for( var i = 0; i < 3; i++ ) {
+				var dx = game.blocks[status.type].pos[i].x;
+				var dy = game.blocks[status.type].pos[i].y;
+				// typeのブロックがとりうる回転のパターン数で割って回転させる数を求める
+				var rot = status.rotate % game.blocks[status.type].rotate;
+				// 現在の状態まで回転させる
+				// 理屈的には90°(1/2pi)ずつアフィン変換で回転させていく
+				// x =  x * cos( 1/2pi ) + y * sin( 1/2pi ) = y
+				// y = -x * sin( 1/2pi ) + y * cos( 1/2pi ) = -x
+				// ※cos( 1/2pi ) は0, sin( 1/2pi )は1
+				// なので計算を省略してx = y, y = -xと簡単に書ける
+				for( var j = 0; j < rot; j++ ) {
+					var nx = dx;
+					var ny = dy;
+					dx = ny;
+					dy = -nx;
+				}
+				// 既にブロックが置かれている場合は終了
+				if( game.BLOCK_TYPE.NO_BLOCK < this._field[status.x + dx][status.y + dy] &&
+					this._field[status.x + dx][status.y + dy] <= game.BLOCK_TYPE.WALL ) return false;
+				if( action ) {
+					this._field[status.x + dx][status.y + dy] = game.BLOCK_TYPE.NO_BLOCK;
+				}
+			}
+
+			// actionがfalseの時にここまで来たら、今度はtrueにして本当に置く
+			if( !action ) {
+				this.putBlock( status, true );
+			}
+
+			return true;
+		},
+		deleteBlock : function( status ) {
+			this._field[x][y] = game.BLOCK_TYPE.NO_BLOCK;
+			for( var i = 0; i < 3; i++ ) {
+				var dx = game.blocks[status.type].pos[i].x;
+				var dy = game.blocks[status.type].pos[i].y;
+				var rot = status.rotate % game.blocks[status.type].rotate;
+				// 現在の状態まで回転させる
+				// 理屈的には90°(1/2pi)ずつアフィン変換で回転させていく
+				// x =  x * cos( 1/2pi ) + y * sin( 1/2pi ) = y
+				// y = -x * sin( 1/2pi ) + y * cos( 1/2pi ) = -x
+				// ※cos( 1/2pi ) は0, sin( 1/2pi )は1
+				// なので計算を省略してx = y, y = -xと簡単に書ける
+				for( var j = 0; j < rot; j++ ) {
+					var nx = dx;
+					var ny = dy;
+					dx = ny;
+					dy = -nx;
+				}
+				this._field[status.x + dx][status.y + dy] = game.BLOCK_TYPE.NO_BLOCK;
+			}
 		}
 	} );
 
+	// ゲームのメインクラス
+	game.Sirtet = enchant.Class.create( {
+		// ゲームの初期化をする
+		initialize : function() {
+			this.field = new game.Field();
+			this.putFlg = false;
+			this.gameOverFlg = false;
+			this.current = new game.Status();
+		},
+		init : function() {
+			this.field.init();
+			this.putFlg = false;
+			this.gameOverFlg = false;
+			this.generateBlock();
+		},
+		// ブロックを生成する
+		generateBlock : function() {
+			if( this.gameOverFlg ) return;
+			this.current.pos.x = Math.floor( game.FIELD_W / 2 );
+			this.current.pos.y = game.FIELD_H + 1;
+			this.current.type = Math.floor( Math.random() * ( game.BLOCK_TYPE_NUM - 1 ) ) + 1;
+			this.current.rotate = Math.floor( Math.random() * 4 );
 
-	/**
-	 * オブジェクト指向的にはBlockやFieldのメソッドにした方がいいものや
-	 * BlockのFactoryとか作った方がいいものもありそうなので後で考える
-	 */
-	// ブロックを生成する
-	game.generateBlock = function() {
-		// TODO: 実装
-	};
-
-	// ブロックをフィールドに設置する
-	game.putBlock = function( status, action ) {
-		// TODO: 実装
-		var ret = false;
-		return ret;
-	};
-
-	// ブロックを落下させる
-	game.fallDownBlock = function() {
-		// TODO: 実装
-	};
-
-	// ブロックを削除する
-	game.deleteBlock = function( status ) {
-		// TODO: 実装
-	};
-
-	// ラインを削除する
-	game.deleteLine = function() {
-		// TODO: 実装
-	};
-	game.onGameOver = function() {
-		// TODO: 実装
-	};
+			// 生成したブロックを置けるか試して、置けない場合はゲームオーバー
+			if( !this.putBlock( this.current, false ) && this.putFlg ) {
+				this.gameOver();
+			}
+			this.putFlg = true;
+		},
+		// ブロックをフィールドに設置する
+		putBlock : function( status, action ) {
+			if( this.gameOverFlg ) return false;
+			return this.field.putBlock( status, action );
+		},
+		// ブロックを落下させる
+		fallDownBlock : function() {
+			// TODO: 実装
+		},
+		// ブロックを削除する
+		deleteBlock : function( status ) {
+			if( this.gameOverFlg ) return;
+			this.field.deleteBlock( status );
+		},
+		// ラインを削除する
+		deleteLine : function() {
+			if( this.gameOverFlg ) return;
+			// TODO: 実装
+		},
+		processGame : function() {
+			
+		},
+		gameOver : function() {
+			// TODO: 実装
+		},
+		onEnterFrame : function() {
+			while( !this.gameOverFlg ) {
+				this.processGame();
+			}
+		}
+	} );
 	
 
 
 //    game.preload("chara1.png");
     
     game.onload = function(){
-    	// TODO: ゲームの初期化
-		field = new game.Field();
+		sirtet = new game.Sirtet();
 		
-		game.rootScene.addChild( field );
+		game.rootScene.addChild( sirtet.field );
 //        bear = new Sprite(32, 32);
 //        bear.image = game.assets["chara1.png"];
 //        bear.x = 0;
